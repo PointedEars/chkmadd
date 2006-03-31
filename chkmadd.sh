b96815a8be0827b6efd8cf1a4a04e73d5f7d63aa
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2006-03-31 02:00:00 +0200

    chkmadd: Updated license address

--- chkmadd.sh
+++ chkmadd.sh
@@ -21,7 +21,8 @@ latest_uri='http://PointedEars.de/tools/network/chkmadd/'
 #
 #     You should have received a copy of the GNU General Public License
 #     along with this program; if not, write to the Free Software
-#     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+#     USA.
 #
 ## Standard shell script disclaimer blurb thing:
 ##
